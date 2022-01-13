dac7cc0c7d69a6808a1ecc426c2d1f45a4093bb1
bean <bkung188@gmail.com>
2022-01-13 01:34:20 -0800

    added HTML

---
+++ index.js
@@ -0,0 +1,15 @@
+const Engineer = require('./lib/engineer');
+const Manager = require('./lib/manager');
+const Intern = require('./lib/intern');
+
+const inquirer = require('inquirer');
+const fs = require('fs');
+
+const generateHTML = require('./src/generateHTML');
+
+const teamArray = [];
+
+function newManager() {
+    
+}
+
